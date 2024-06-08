73710fe40fbcd50fa6a2fa90db56cafcc0116d17
Sayid Rofi <sayidrofi291@gmail.com>
2024-06-08 12:09:51 +0700

    frist commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  darkMode: 'class',
+          theme: {
+            extend: {
+              spacing: {
+                },
+                fontFamily: {
+                    Poppins: ['Poppins']
+                },
+                colors: {
+                    Tombol: '#7f56d9'
+                },
+          },
+        },ins: [],
+}
+
